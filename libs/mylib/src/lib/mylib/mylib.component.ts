a8315f57317a04ef984e1854e8bef0dd99b8dca4
Colum Ferry <cferry09@gmail.com>
2023-11-10 15:48:35 +0000

    feat: reproduction

---
+++ libs/mylib/src/lib/mylib/mylib.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'ng-control-flow-mig-repro-mylib',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './mylib.component.html',
+  styleUrls: ['./mylib.component.css'],
+})
+export class MylibComponent {}
